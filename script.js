10741eb3e844912da2588e515b1775b605c727a7
IrynkaKo <olenkaolenich@gmail.com>
2023-03-08 10:55:20 +0200

    Initial commmit

---
+++ script.js
@@ -0,0 +1,8 @@
+const birthDate = '1991-04-07';
+
+const firstName = prompt('Enter your name');
+const lastName = prompt('Enter your last name');
+const fullName = prompt(`Full Name: ${firstName} ${lastName}`);
+
+alert(`Hello ${fullName}! Your birth date is ${birthDate}`);
+ 
