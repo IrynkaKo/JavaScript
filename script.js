59efadc6a6fda13b65ede4bfc3b9feb594c3abe8
IrynkaKo <olenkaolenich@gmail.com>
2023-03-11 18:53:35 +0200

    Changes

--- script.js
+++
@@ -1,8 +0,0 @@
-const birthDate = '1991-04-07';
-
-const firstName = prompt('Enter your name');
-const lastName = prompt('Enter your last name');
-const fullName = prompt(`Full Name: ${firstName} ${lastName}`);
-
-alert(`Hello ${fullName}! Your birth date is ${birthDate}`);
- 
