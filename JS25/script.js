75e9aee5715a6f5ed38e4bde93a367bc63de2935
IrynkaKo <olenkaolenich@gmail.com>
2023-03-21 22:28:24 +0200

    New commit

--- JS25/script.js
+++ JS25/script.js
@@ -1,91 +1,51 @@
-let bigNumber = prompt("Enter first number");
-   let smallNumber = prompt("Enter second number");
-    
-    function summingNumbers(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return "Error: both parameters must be numbers";
-  }
-  return Number(a) + Number(b);
+const Obj = {};
+const Obj1 = { name: "Lily", age: 33 };
+
+
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0;
 }
 
- let sum = summingNumbers(bigNumber, smallNumber);
- alert(` ${sum}`);
+console.log(isObjectEmpty(Obj));     
+console.log(isObjectEmpty(Obj1)); 
 
 
-  
-  
-   let firstNumber = prompt("Enter first number");
-   let secondNumber = prompt("Enter second number");
 
-   function divideNumbers(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return "Error: both parameters must be numbers";
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
   }
-  if (a === 0 || b === 0) {
-    return "Error: division by zero";
+
+  sayHello() {
+    return `Привіт, я ${this.name}, мені ${this.age} років`;
   }
-  return a / b;
 }
 
-    let result = divideNumbers(firstNumber, secondNumber);
-    alert(`${result}`);
+const user = new User("Ірина", 31);
+console.log(user.sayHello());
 
 
-    let newNumber = prompt("Enter first number");
-    let newnewNumber = prompt("Enter second number");
-    
-    function multiplyNumbers(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return "Error: both parameters must be numbers";
-  }
-  return a * b ;
-}
-
- let multiply = multiplyNumbers(newNumber, newnewNumber);
- alert(` ${multiply}`);
-
-
-
-    let lastNumber = prompt("Enter first number");
-    let verylastNumber = prompt("Enter second number");
-    
-    function minusNumbers(a, b) {
-    if (isNaN(a) || isNaN(b)) {
-    return "Error: both parameters must be numbers";
+const calculator = {
+  num1: null,
+  num2: null,
+  ask: function() {
+    this.num1 = +prompt('Введіть перше число:');
+    this.num2 = +prompt('Введіть друге число:');
+  },
+  sum: function() {
+    if (this.num1 === null || this.num2 === null) {
+      console.log('Спочатку введіть числа');
+    } else {
+      console.log(`Сума: ${this.num1 + this.num2}`);
     }
-
-    if (b > a){
-    return ("Are you sure you want to continue?");
-    }
-
-    return a - b;
-}
-
-   let check = minusNumbers(lastNumber, verylastNumber);
-   alert(` ${check}`);
-
-
-
-   const numbers = [  860, 140, 53, 74, 867 ,100 ,11 ,20,699,921];
-   function findMin(numbers) {
-   let min = numbers[0];
-   for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] < min) {
-      min = numbers[i];
+  },
+  mul: function() {
+    if (this.num1 === null || this.num2 === null) {
+      console.log('Спочатку введіть числа');
+    } else {
+      console.log(`Добуток: ${this.num1 * this.num2}`);
     }
   }
-  return min;
-}
+};
 
-function findMax(numbers) {
-  let max = numbers[0]; 
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-      max = numbers[i];
-    }
-  }
-  return max;
-}
-
-console.log(findMin(numbers)); 
-console.log(findMax(numbers));
